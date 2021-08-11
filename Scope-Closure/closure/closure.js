47db7d853f172c72f9a2b09a5e63e63a448fbfbd
Ontodivarius <saledorlm@yahoo.com>
2021-08-10 22:09:43 -0400

    Closures

--- Scope-Closure/closure/closure.js
+++ Scope-Closure/closure/closure.js
@@ -8,3 +8,19 @@ const moneyBox = (coins) => {
 
 moneyBox(5);
 moneyBox(10);
+
+
+const moneyBox = () => {
+    var saveCoins = 0;
+    const countCoins = (coins) => {
+        saveCoins += coins;
+        console.log(`MoneyBox: $${saveCoins}`);
+    }
+    return countCoins;
+}
+
+let myMoneyBox = moneyBox();
+
+myMoneyBox(4);
+myMoneyBox(6);
+myMoneyBox(10);
---
+++ Scope-Closure/closure/lexical.js
@@ -0,0 +1,18 @@
+const buildCount = (i) =>{
+    let count = i;
+    const displaycount = () =>{
+        console.log(count++);
+    }
+    return displaycount;
+}
+
+
+const myCount = buildCount(1);
+
+myCount();
+myCount();
+myCount();
+
+const myOtherCount = buildCount(10);
+myOtherCount();
+myOtherCount();
